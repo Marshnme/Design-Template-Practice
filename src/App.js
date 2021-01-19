6d55c732ff6119035985a5ca7d5bde50d7c2439e
Joshua Holtsclaw <jildsmiles@yahoo.com>
2021-01-18 22:25:04 -0500

    basic HTML setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>location</li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        <div>
+          <img href="#"></img>
+          <h2>The leader in interactive VR</h2>
+          <p>Anim ea id irure cupidatat culpa veniam eiusmod duis tempor in aliquip eiusmod pariatur qui. Eu nostrud deserunt ex eiusmod aliqua anim minim sunt cillum exercitation in. Amet quis in eu pariatur esse sunt quis velit sint dolore laboris nisi nulla. Duis ea laborum magna officia. In nisi occaecat aute Lorem amet tempor irure pariatur reprehenderit laborum aliqua.</p>
+        </div>
+        <div>
+          <h2>Our Creations</h2>
+          <img href="#"></img>
+          <img href="#"></img>
+          <img href="#"></img>
+          <img href="#"></img>
+          <img href="#"></img>
+          <img href="#"></img>
+          <img href="#"></img>
+          <img href="#"></img>
+          <img href="#"></img>
+          <button>SEE ALL</button>
+        </div>
+        <footer>
+          <h3>Loopstudios</h3>
+          <div>
+            <ul>
+              <li>About</li>
+              <li>Careers</li>
+              <li>Events</li>
+              <li>Products</li>
+              <li>Support</li>
+            </ul>
+          </div>
+          <div>
+            {/* SOCIAL LOGOS  */}
+          </div>
+        </footer>
+      </main>
+    </body>
   );
 }
 
