580120dd9c74bf391c05e5b29a821e67512f4598
Joshua Holtsclaw <jildsmiles@yahoo.com>
2021-02-01 21:12:52 -0500

    Started desktop flexbox layout

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,6 +9,15 @@ import FromAbove from "../component-imgs/mobile-imgs/image-from-above.jpg";
 import Borealis from "../component-imgs/mobile-imgs/image-pocket-borealis.jpg";
 import Curious from "../component-imgs/mobile-imgs/image-curiosity.jpg";
 import Fisheye from "../component-imgs/mobile-imgs/image-fisheye.jpg";
+import InteractiveDesk from "../component-imgs/desktop-imgs/image-interactive.jpg";
+import DeepEarthDesk from "../component-imgs/desktop-imgs/image-deep-earth.jpg";
+import NightArcadeDesk from "../component-imgs/desktop-imgs/image-night-arcade.jpg";
+import SoccerVRDesk from "../component-imgs/desktop-imgs/image-soccer-team.jpg";
+import RacecarDesk from "../component-imgs/desktop-imgs/image-grid.jpg";
+import FromAboveDesk from "../component-imgs/desktop-imgs/image-from-above.jpg";
+import BorealisDesk from "../component-imgs/desktop-imgs/image-pocket-borealis.jpg";
+import CuriousDesk from "../component-imgs/desktop-imgs/image-curiosity.jpg";
+import FisheyeDesk from "../component-imgs/desktop-imgs/image-fisheye.jpg";
 import { AiOutlineFacebook as FBLogo   } from "react-icons/ai";
 import { AiOutlineTwitter as TwitterLogo  } from "react-icons/ai";
 import { FaPinterestSquare as PinterestLogo } from "react-icons/fa";
@@ -33,25 +42,27 @@ const LandingPage = () =>{
               </div>
             </nav>
             <div className="header-text-parent">
-              <h1 className="header-text">Immersive experiences that<br/> deliver</h1>
+              <h1 className="header-text">Immersive experiences that deliver</h1>
             </div>
           </header>
           <main>
             <div className="interactive-parent">
               <img className="interactive-img" src={Interactive} alt="Man in VR headset"></img>
-              <h2 className="interactive-header">The leader in interactive VR</h2>
-              <p>Anim ea id irure cupidatat culpa veniam eiusmod duis tempor in aliquip eiusmod pariatur qui. Eu nostrud deserunt ex eiusmod aliqua anim minim sunt cillum exercitation in. Amet quis in eu pariatur esse sunt quis velit sint dolore laboris nisi nulla. Duis ea laborum magna officia. In nisi occaecat aute Lorem amet tempor irure pariatur reprehenderit laborum aliqua.</p>
+              <div>
+                <h2 className="interactive-header">The leader in interactive VR</h2>
+                <p>Anim ea id irure cupidatat culpa veniam eiusmod duis tempor in aliquip eiusmod pariatur qui. Eu nostrud deserunt ex eiusmod aliqua anim minim sunt cillum exercitation in. Amet quis in eu pariatur esse sunt quis velit sint dolore laboris nisi nulla. Duis ea laborum magna officia. In nisi occaecat aute Lorem amet tempor irure pariatur reprehenderit laborum aliqua.</p>
+              </div>
             </div>
             <div className="creations-parent">
               <h2>Our Creations</h2>
-              <img className="DeepEarth creations" src={DeepEarth} alt="Earth view from space"></img>
-              <img className="NightArcade creations" src={NightArcade} alt="Skeeball score rings"></img>
-              <img className="SoccerVR creations" src={SoccerVR} alt="Soccer player mid run"></img>
-              <img className="Racecar creations" src={Racecar} alt="Closeup of tire on race car"></img>
-              <img className="FromAbove creations" src={FromAbove} alt="Race track from top view"></img>
-              <img className="Borealis creations" src={Borealis} alt="Stars and Space"></img>
-              <img className="Curious creations"  src={Curious} alt="Curiosity rover"></img>
-              <img className="Fisheye creations" src={Fisheye} alt="warped picture of nature"></img>
+              <div className="DeepEarth creations"  alt="Earth view from space"></div>
+              <div className="NightArcade creations"  alt="Skeeball score rings"></div>
+              <div className="SoccerVR creations"  alt="Soccer player mid run"></div>
+              <div className="Racecar creations"  alt="Closeup of tire on race car"></div>
+              <div className="FromAbove creations"  alt="Race track from top view"></div>
+              <div className="Borealis creations"  alt="Stars and Space"></div>
+              <div className="Curious creations"  alt="Curiosity rover"></div>
+              <div className="Fisheye creations"  alt="warped picture of nature"></div>
               <button>SEE ALL</button>
             </div>
             <footer className="footer-parent">
